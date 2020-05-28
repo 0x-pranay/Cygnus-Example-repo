86b39ae7f22d85b2a849dfcfb525cd348266d78d
Pranay Velisoju <pranay@soal.io>
2020-05-28 15:35:28 +0530

    add a file to explain using faker

---
+++ using_faker.js
@@ -0,0 +1,7 @@
+{
+  "name": "Adaline Carroll",
+  "email": "Albina.Ledner@gmail.com",
+  "address": "545 Fritsch Locks",
+  "bio": "dolorem officiis doloribus ut",
+  "image": "https://s3.amazonaws.com/uifaces/faces/twitter/joelhelin/128.jpg"
+}
